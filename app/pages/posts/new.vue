6dd75b6c8a44560a55ff5562e442c32f527cb97d
Hajime_Kurita <h_kurita@rise-force.co.jp>
2020-11-17 18:11:19 +0900

    Plugin not found: C:\Users\rifo015\footballer-app\plugins\element-uiの修正（修正しきれず,vue add elementeとelement-uiの再インストールをしたがかいしょうせず）

--- app/pages/posts/new.vue
+++ app/pages/posts/new.vue
@@ -1,58 +1,57 @@
 <template>
-<section class="container posts-page">
-    <el-card style="flex:1">
-        <div slot="header" class="clearfix">
-            <el-input placeholder="選手名" v-model="formData.title" />
-        </div>
-        <div>
-            <el-input placeholder="選手の特徴" type="textarea" rows="15"
-            v-model="formData.body" />
-        </div>
-        <div class="text-right" style="margin-top:16px">
-            <el-button type="primary" @click="publish" round>
-                <span class="el-icon-upload2" />
-                <span>Publish</span>
-            </el-button>
-        </div>
+  <section class="container posts-page">
+    <el-card style="flex: 1">
+      <div slot="header" class="clearfix">
+        <el-input placeholder="タイトルを入力" v-model="formData.title" />
+      </div>
+      <div>
+        <el-input placeholder="本文を入力……" type="textarea" rows="15" v-model="formData.body" />
+      </div>
+      <div class="text-right" style="margin-top: 16px;">
+        <el-button type="primary" @click="publish" round>
+          <span class="el-icon-upload2" />
+          <span>Publish</span>
+        </el-button>
+      </div>
     </el-card>
-</section>
+  </section>
 </template>
 
 <script>
-import {mapGetters,mapActions} from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 
-export default{
-    asyncData({redirect,store}){
-        if(!store.getters['user']){
-            redirect('/')
-        }
-        return{
-            formData:{
-                title:'',
-                body:''
-            }
-        }
-    },
-    computed:{
-        ...mapGetters(['user'])
-    },
-    methods:{
-        async public(){
-            const payload={
-                user:this.user,
-                ...this.formData,
-            }
-            await this.publishPost({payload})
-            this.$router.push('/posts')
-        },
-        ...mapActions('users',['updateUser']),
-        ...mapActions('posts',['publishPost'])
+export default {
+  asyncData({ redirect, store }) {
+    if (!store.getters['user']) {
+      redirect('/')
     }
- }
+    return {
+      formData: {
+        title: '',
+        body: ''
+      }
+    }
+  },
+  computed: {
+    ...mapGetters(['user'])
+  },
+  methods: {
+    async publish() {
+      const payload = {
+        user: this.user,
+        ...this.formData
+      }
+      await this.publishPost({ payload })
+      this.$router.push('/posts')
+    },
+    ...mapActions('users', ['updateUser']),
+    ...mapActions('posts', ['publishPost'])
+  }
+}
 </script>
 
 <style>
-.posts-page .eltable__row{
-    cursor:pointer;
+.posts-page .el-table__row {
+  cursor: pointer;
 }
 </style>
